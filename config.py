c5431d6ad5fc89bcfdb80d4e343e2e15121b50db
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-09-23 04:46:02 +0300

    removing every file

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'mysecretkey')  # For JWT secret key
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  # Database connection
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'anothersecretkey')  # For JWT tokens
